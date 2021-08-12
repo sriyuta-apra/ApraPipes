2bed307648735302e5dbdd7563cf6af0e8e43083
sriyutas <sriyutas@apra.in>
2021-08-12 16:32:51 +0530

    adding Denoise

---
+++ base/include/Denoise.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Module.h"
+#include "/home/developer/Projects/sfml/rnnoise/include/rnnoise.h"
+
+class DenoiseProps : public ModuleProps
+{
+public:
+    DenoiseProps(int _samplerate = 48000, int _channel = 1, int _byteDepth = 2) : ModuleProps()
+    {
+        sampleRate = _samplerate;
+        channel = _channel;
+        byteDepth = _byteDepth;
+    }
+    int sampleRate;
+    int channel;
+    int byteDepth;
+};
+
+class Denoise : public Module
+{
+public:
+    Denoise(DenoiseProps _props);
+    virtual ~Denoise();
+    virtual bool init();
+    virtual bool term();
+
+protected:
+#define FRAME_SIZE 480
+    bool process(frame_container &frames);
+    void addInputPin(framemetadata_sp &metadata, string &pinId);
+    bool validateInputPins();
+    bool validateInputOutputPins();
+    bool validateOutputPins();
+
+private:
+    void setMetadata(framemetadata_sp &metadata);
+    class Detail;
+    boost::shared_ptr<Detail> mDetail;
+    DenoiseProps props;
+    std::string mDenoiseRawAudio;
+    std::string mOutputPinId;
+    std::string mOutputPinId1;
+    const size_t CHUNK_SIZE = 480 * 2;
+    DenoiseState *st;
+    frame_sp mTempAudio;
+    frame_sp fiveSecFrame;
+    size_t mPrevSampleSize;
+    size_t CHUNK_COUNT = 480;
+    int counter = 0; 
+    int check = counter + 30;
+};
---
+++ base/src/Denoise.cpp
@@ -0,0 +1,170 @@
+#include "Denoise.h"
+// #include "opencv2/core/core.hpp"
+// #include "opencv2/highgui/highgui.hpp"
+// #include "opencv2/imgproc/imgproc.hpp"
+
+Denoise::Denoise(DenoiseProps _props) : Module(TRANSFORM, "Denoise", _props), mPrevSampleSize(0)
+{
+	st = rnnoise_create(NULL);
+	auto mDenoiseRawAudio = framemetadata_sp(new FrameMetadata(FrameMetadata::FrameType::AUDIO));
+	mOutputPinId = addOutputPin(mDenoiseRawAudio);
+	mOutputPinId1 = addOutputPin(mDenoiseRawAudio);
+}
+
+Denoise::~Denoise()
+{
+	rnnoise_destroy(st);
+}
+bool Denoise::validateInputPins()
+{
+	framemetadata_sp metadata = getFirstInputMetadata();
+	FrameMetadata::FrameType frameType = metadata->getFrameType();
+	if (frameType != FrameMetadata::AUDIO)
+	{
+		LOG_ERROR << "<" << getId() << ">::validateInputPins input frameType is expected to be AUDIO. Actual<" << frameType << ">";
+		return false;
+	}
+
+	FrameMetadata::MemType memType = metadata->getMemType();
+	if (memType != FrameMetadata::MemType::HOST)
+	{
+		LOG_ERROR << "<" << getId() << ">::validateInputPins input memType is expected to be host. Actual<" << memType << ">";
+		return false;
+	}
+	if (getNumberOfInputPins() != 1)
+	{
+		LOG_ERROR << "<" << getId() << ">::validateOutputPins size is expected to be 1. Actual<" << getNumberOfInputPins() << ">";
+		return false;
+	}
+
+	return true;
+}
+
+bool Denoise::validateInputOutputPins() //validateINputoutput
+{
+	if (getNumberOfOutputPins() != 2)
+	{
+		LOG_ERROR << "<" << getId() << ">::validateOutputPins size is expected to be 1. Actual<" << getNumberOfOutputPins() << ">";
+		return false;
+	}
+	return true;
+}
+
+bool Denoise::validateOutputPins()
+{
+	framemetadata_sp metadata = getFirstOutputMetadata();
+	auto mOutputFrameType = metadata->getFrameType();
+	if (mOutputFrameType != FrameMetadata::AUDIO)
+	{
+		LOG_ERROR << "<" << getId() << ">::validateOutputPins input frameType is expected to be AUDIO. Actual<" << mOutputFrameType << ">";
+		return false;
+	}
+
+	FrameMetadata::MemType memType = metadata->getMemType();
+	if (memType != FrameMetadata::MemType::HOST)
+	{
+		LOG_ERROR << "<" << getId() << ">::validateOutputPins input memType is expected to be host. Actual<" << memType << ">";
+		return false;
+	}
+
+	return true;
+}
+void Denoise::addInputPin(framemetadata_sp &metadata, string &pinId)
+{
+	Module::addInputPin(metadata, pinId);
+}
+
+bool Denoise::process(frame_container &frames)
+{
+
+	//take all incomming input frames and beak into 480 chunks
+	auto frame = frames.cbegin()->second;
+	if (isFrameEmpty(frame))
+	{
+		return true;
+	}
+
+	auto extraSampleSize = (mPrevSampleSize + frame->size()) % (480 * sizeof(int16_t));
+	auto a = frame->size();
+	auto outSize = (mPrevSampleSize + frame->size()) - (extraSampleSize);
+	auto outFrame = makeFrame(outSize);
+	LOG_INFO << frame->size();
+	int chunks = (mPrevSampleSize + frame->size()) / CHUNK_SIZE;
+
+	if (mPrevSampleSize != 0)
+	{
+		memcpy(outFrame->data(), mTempAudio->data(), mPrevSampleSize);
+	}
+	memcpy(outFrame->data() + mPrevSampleSize, frame->data(), frame->size() - extraSampleSize);
+
+	//A float frame
+	auto outFrame_float = makeFrame(outSize * (sizeof(float) - sizeof(int16_t)));
+	auto denoise_size = (outFrame->data() + mPrevSampleSize);
+
+	//convert int16 to float
+	// cv::Mat M1(1, (outFrame->size()) / 2, CV_16S, outFrame->data());
+	// cv::Mat M2(1, (outFrame->size()) / 2, CV_32F, outFrame_float->data());
+	// M1.convertTo(M2, CV_32F);
+	int16_t finalOutput[CHUNK_COUNT * chunks];
+
+	for (int i = 0; i < chunks; i++)
+	{
+		int16_t input[CHUNK_COUNT];
+		float x[CHUNK_COUNT];
+		memcpy(input, outFrame->data() + (i * CHUNK_SIZE), CHUNK_SIZE);
+		for (int index = 0; index < CHUNK_COUNT; index++)
+		{
+			x[index] = input[index];
+		}
+		//rnnoise_process_frame(st, static_cast<float *>(outFrame_float->data()) + i * CHUNK_COUNT, static_cast<float *>(outFrame_float->data()) + i * CHUNK_COUNT);
+		rnnoise_process_frame(st, x, x);
+		for (int index = 0; index < CHUNK_COUNT; index++)
+		{
+			finalOutput[index + i * CHUNK_COUNT] = x[index];
+		}
+	}
+	memcpy(outFrame->data(), finalOutput, CHUNK_SIZE * chunks);
+
+	// M2.convertTo(M1, CV_16S);
+
+	// COPY extra samples
+	memcpy(mTempAudio->data(), frame->data() + (frame->size() - extraSampleSize), extraSampleSize);
+	mPrevSampleSize = extraSampleSize;
+	//frames.insert(make_pair(mOutputPinId, outFrame)); //send denoised frame
+	//frames.insert(make_pair(mOutputPinId1, frame));	  //send normal frame
+
+	//send 5 sec chunk
+	memcpy(fiveSecFrame->data() + counter * outFrame->size(), outFrame->data(), outFrame->size());
+	counter = counter + 1;
+	LOG_INFO << counter;
+	if (counter == check)
+	{
+		LOG_INFO << "I AM SENDING A CHUNK OF FRAME";
+		frames.insert(make_pair(mOutputPinId, fiveSecFrame));
+		send(frames);
+		check = check + 50;
+	}
+
+	return true;
+}
+
+void Denoise::setMetadata(framemetadata_sp &metadata)
+{
+	//auto samplerate =  get.samplerate();
+	//auto channel = get.channel();
+	//auto bitdepth = get.bit();
+}
+
+bool Denoise::init()
+{
+	auto ret = Module::init();
+	mTempAudio = makeFrame(480 * 2);
+	auto size = sizeof(float) * 2400000;
+	fiveSecFrame = makeFrame(size);
+	return ret;
+}
+
+bool Denoise::term()
+{
+	return Module::term();
+}
